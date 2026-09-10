127e9922b18356494d40f701b06483ec3f4b3954
Le Duy Anh <25ad11004@student.hcmute.edu.vn>
2026-09-10 22:39:43 +0700

    score range

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main() {
     int n;
-    float a[50];
+    float s[50];
     int passed = 0;
     int failed = 0;
-    float highest = a[0];
-    float lowest = a[0];
+    float highest = s[0];
+    float lowest = s[0];
     int countExcellent = 0;
+    int a, b;
+    int hs = 0;
     cout << "nhap n: ";
     cin >> n;
     if (n <= 2 || n >= 20) {
@@ -20,34 +22,34 @@ int main() {
 
     for (int i = 0; i < n; i++) {
         cout << "so diem hoc sinh " << i + 1 << ": ";
-        cin >> a[i];
-        while (a[i] < 0 || a[i] > 10) {
+        cin >> s[i];
+        while (s[i] < 0 || s[i] > 10) {
             cout << "diem khong phu hop, nhap lai tu 0 den 10: ";
-            cin >> a[i];
+            cin >> s[i];
         }
     }
     cout << "\n===== diem hoc sinh =====\n";
 
     for (int i = 0; i < n; i++) {
-        cout << "hoc sinh " << i + 1 << ": " << a[i] << endl;
+        cout << "hoc sinh " << i + 1 << ": " << s[i] << endl;
     }
 
     cout << "\n===== RESULTS =====\n";
 
     for (int i = 0; i < n; i++) {
-        if (a[i] >= 5) {
+        if (s[i] >= 5) {
             cout << "hoc sinh " << i + 1 << ": "
-                << a[i] << " PASS\n";
+                << s[i] << " PASS\n";
         }
         else {
             cout << "hoc sinh " << i + 1 << ": "
-                << a[i] << " FAIL\n";
+                << s[i] << " FAIL\n";
         }
     }
 
 
     for (int i = 0; i < n; i++) {
-        if (a[i] >= 5) {
+        if (s[i] >= 5) {
             passed++;
         }
         else {
@@ -65,30 +67,30 @@ int main() {
 
 
     for (int i = 1; i < n; i++) {
-        if (a[i] > highest) {
-            highest = a[i];
+        if (s[i] > highest) {
+            highest = s[i];
         }
 
-        if (a[i] < lowest) {
-            lowest = a[i];
+        if (s[i] < lowest) {
+            lowest = s[i];
         }
     }
 
     cout << "\n===== CLASSIFICATION =====\n";
 
     for (int i = 0; i < n; i++) {
-        cout << "Student " << i + 1 << ": " << a[i] << " -> ";
+        cout << "Student " << i + 1 << ": " << s[i] << " -> ";
 
-        if (a[i] >= 9) {
+        if (s[i] >= 9) {
             cout << "Excellent";
         }
-        else if (a[i] >= 8) {
+        else if (s[i] >= 8) {
             cout << "Very Good";
         }
-        else if (a[i] >= 6.5) {
+        else if (s[i] >= 6.5) {
             cout << "Good";
         }
-        else if (a[i] >= 5) {
+        else if (s[i] >= 5) {
             cout << "Average";
         }
         else {
@@ -101,13 +103,26 @@ int main() {
     cout << "\nHighest score: " << highest << endl;
     cout << "Lowest score: " << lowest << endl;
     for (int i = 0; i < n; i++) {
-        if (a[i] >= 9.0) {
+        if (s[i] >= 9.0) {
             countExcellent++;
         }
     }
 
     cout << "so hoc sinh xuat sac: "
     << countExcellent << endl;
+
+    cout << "nhap a: ";
+    cin >> a;
+    cout << "nhap b: ";
+    cin >> b;
+    for(int i = 0; i < n;i++){
+        if(s[i] >= a && s[i] <= b){
+            hs++;
+        }
+    }
+    cout << "tong so hoc sinh co diem tu a den b la: " << hs;
+
+
 
     return 0;
 }
